30cb14c427e3fba9727ba42429c4fe6f3f39a571
Nomura Suzume <suzume315@g00.g1e.org>
2021-12-03 20:16:50 +0900

    added test case for httpfilter/sythesis (now only for httprouter support)

---
+++ synthesis/noauth_test.go
@@ -0,0 +1,22 @@
+package synthesis
+
+import (
+	"bytes"
+	. "gopkg.in/check.v1"
+	"net/http"
+	"net/http/httptest"
+)
+
+func (s *filterTestSuite) TestNoAuth(c *C) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/ok", bytes.NewBufferString(""))
+	NoAuth(s.echoResponder)(w, r, nil)
+	c.Check(w.Code, Equals, http.StatusOK)
+}
+
+func (s *filterTestSuite) TestFalse(c *C) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/ok", bytes.NewBufferString(""))
+	False(s.echoResponder)(w, r, nil)
+	c.Check(w.Code, Equals, http.StatusForbidden)
+}
---
+++ synthesis/setup_test.go
@@ -0,0 +1,31 @@
+package synthesis
+
+import (
+	"github.com/julienschmidt/httprouter"
+	. "gopkg.in/check.v1"
+	"net/http"
+	"testing"
+)
+
+func init() {
+	Suite(&filterTestSuite{})
+}
+
+type filterTestSuite struct {
+}
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *filterTestSuite) SetUpTest(_ *C) {
+}
+
+func (s *filterTestSuite) nullHandler(_ http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	return
+}
+
+//echoResponder is a test stub for POST requests, which makes echo of a post body
+func (s *filterTestSuite) echoResponder(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var a []byte
+	_, _ = r.Body.Read(a)
+	_, _ = w.Write(a)
+}
