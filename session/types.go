8bdaca127178e9db876f34a977ed6943516ca49b
Nomura Suzume <suzume315@g00.g1e.org>
2021-12-03 19:55:42 +0900

    rearrange missing types.go file

---
+++ session/types.go
@@ -0,0 +1,15 @@
+package session
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+//Provider represents authentication provider.
+type Provider interface {
+	Auth(handle httprouter.Handle) httprouter.Handle
+	RegisterClient(r *http.Request) (string, error)
+	RevokeClient(r *http.Request) error
+	GetAccessToken() string     //GetAccessToken returns temporary access token for protected resources
+	Log(logLine string) []error //write log with preset log writer
+}
